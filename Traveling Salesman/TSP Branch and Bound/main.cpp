c96112675d04aa0a863b4b46a4ac5211ce6ce57d
AaronBattershell <soccerman0010@gmail.com>
2014-09-05 12:31:12 -0400

    Added additional documentation

--- Traveling Salesman/TSP Branch and Bound/main.cpp
+++ Traveling Salesman/TSP Branch and Bound/main.cpp
@@ -3,12 +3,18 @@
 #include "tsp.hpp"
 #include <iostream>
 
+/*
+/ Input is specified via the command line. This file expects
+/ a single integer, the number of nodes wanted to run a test
+/ on. 
+*/
+
 int main(int argc, char** args) {
 	if (argc == 2) {
 		tsp tspProblem(args[1]);
 		tspProblem.printSolution();
 	} else {
-		std::cout << "Invalid input : string argument expected" << std::endl;
+		std::cout << "Invalid input : integer argument expected" << std::endl;
 	}
 	
 	return 0;
